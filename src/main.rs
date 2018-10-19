e8615e11b8851a2ae8b7f95f23ebf2cf49980b0d
Sean Cross <sean@xobs.io>
2018-10-19 19:13:29 +0800

    main: add -l option, rename /previous.json to /log/previous.json
    Rename logfiles to something that isn't in use.
    
    Add the option to log stdin with `-l`.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -124,6 +124,9 @@ pub struct InterfaceState {
     /// Incoming messages (for debugging)
     stdin_log: Vec<String>,
 
+    /// Decide whether to log messages to stdin
+    log_stdin: bool,
+
     /// Logs from the current run (i.e. since "START" was received)
     current_log: Vec<LogMessage>,
 
@@ -356,7 +359,7 @@ fn abort_tests(_: &mut Request) -> IronResult<Response> {
     Ok(Response::with((status::Ok, "Aborting tests".to_string())))
 }
 
-fn stdin_describe(data_arc: &Arc<Mutex<InterfaceState>>, items: Vec<String>) {
+fn stdin_describe(data: &mut InterfaceState, items: Vec<String>) {
     let mut rest = items.clone();
     let class = rest.remove(0).to_lowercase();
     let field = rest.remove(0).to_lowercase();
@@ -370,18 +373,18 @@ fn stdin_describe(data_arc: &Arc<Mutex<InterfaceState>>, items: Vec<String>) {
 
     match class.as_str() {
         "test" => match field.as_str() {
-            "name" => {data_arc.lock().unwrap().test_names.insert(name_lc, value);},
-            "description" => {data_arc.lock().unwrap().test_descriptions.insert(name_lc, value);},
+            "name" => {data.test_names.insert(name_lc, value);},
+            "description" => {data.test_descriptions.insert(name_lc, value);},
             f => eprintln!("Unrecognized field: {}", f),
         },
         "scenario" => match field.as_str() {
-            "name" => {data_arc.lock().unwrap().scenario_names.insert(name_lc, value);},
-            "description" => {data_arc.lock().unwrap().scenario_descriptions.insert(name_lc, value);},
+            "name" => {data.scenario_names.insert(name_lc, value);},
+            "description" => {data.scenario_descriptions.insert(name_lc, value);},
             f => eprintln!("Unrecognized field: {}", f),
         },
         "jig" => match field.as_str() {
-            "name" => {data_arc.lock().unwrap().jig_name = value;},
-            "description" => {data_arc.lock().unwrap().jig_description = value;},
+            "name" => {data.jig_name = value;},
+            "description" => {data.jig_description = value;},
             f => eprintln!("Unrecognized field: {}", f),
         },
         c => eprintln!("Unrecognized class: {}", c),
@@ -393,7 +396,10 @@ fn stdin_monitor(data_arc: Arc<Mutex<InterfaceState>>, logs: Arc<Mutex<Vec<LogMe
     loop {
         let mut line = String::new();
         rx.read_line(&mut line).ok().expect("Unable to read line");
-        data_arc.lock().unwrap().stdin_log.push(line.clone());
+        let ref mut data = *data_arc.lock().unwrap();
+        if data.log_stdin {
+            data.stdin_log.push(line.clone());
+        }
         let line = cfti_unescape(line);
 
         let mut items: Vec<String> = line.split_whitespace().map(|x| x.to_string()).collect();
@@ -402,28 +408,28 @@ fn stdin_monitor(data_arc: Arc<Mutex<InterfaceState>>, logs: Arc<Mutex<Vec<LogMe
         items.remove(0);
 
         match verb.as_str() {
-            "hello" => data_arc.lock().unwrap().server = items.join(" "),
-            "jig" => data_arc.lock().unwrap().jig = items.get(0).unwrap_or(&"No Jig".to_string()).clone(),
-            "scenarios" => data_arc.lock().unwrap().scenarios = items.clone(),
+            "hello" => data.server = items.join(" "),
+            "jig" => data.jig = items.get(0).unwrap_or(&"No Jig".to_string()).clone(),
+            "scenarios" => data.scenarios = items.clone(),
             "scenario" => {
-                data_arc.lock().unwrap().scenario = items.get(0).unwrap_or(&"No Scenario".to_string()).clone();
-                data_arc.lock().unwrap().scenario_state = ScenarioState::Pending;
+                data.scenario = items.get(0).unwrap_or(&"No Scenario".to_string()).clone();
+                data.scenario_state = ScenarioState::Pending;
             },
             "tests" => {
                 let scenario_name = items.remove(0); // Remove the scenario name, which is the first result.
-                data_arc.lock().unwrap().tests.insert(scenario_name, items.clone());
+                data.tests.insert(scenario_name, items.clone());
 
                 // We got a new set of tests, so reset all the test results to "Pending".
-                data_arc.lock().unwrap().test_results.clear();
+                data.test_results.clear();
                 for item in items {
-                    data_arc.lock().unwrap().test_results.insert(item, TestResult::Pending);
+                    data.test_results.insert(item, TestResult::Pending);
                 }
             },
-            "describe" => stdin_describe(&data_arc, items),
+            "describe" => stdin_describe(data, items),
             "ping" => cfti_send(OutgoingMessage::Pong(items.get(0).unwrap_or(&"".to_string()).clone())),
             "start" => {
                 let scenario_name = items.remove(0);
-                let ref mut data = *data_arc.lock().unwrap();
+                let ref mut data = *data;
                 data.scenario_state = ScenarioState::Running;
 
                 // We got a new set of tests, so reset all the test results to "Pending".
@@ -444,7 +450,7 @@ fn stdin_monitor(data_arc: Arc<Mutex<InterfaceState>>, logs: Arc<Mutex<Vec<LogMe
                     Err(e) => {eprintln!("Unable to parse result: {:?}", e); 500},
                 };
 
-                data_arc.lock().unwrap().scenario_state = match result {
+                data.scenario_state = match result {
                     // Only results of 200 to 299 are considered "pass"
                     200 ... 299 => ScenarioState::Pass,
                     _ => ScenarioState::Fail,
@@ -452,22 +458,22 @@ fn stdin_monitor(data_arc: Arc<Mutex<InterfaceState>>, logs: Arc<Mutex<Vec<LogMe
             }
             "running" => {
                 let test_id = items.remove(0);
-                data_arc.lock().unwrap().test_results.insert(test_id, TestResult::Running);
+                data.test_results.insert(test_id, TestResult::Running);
             },
             "pass" => {
                 let test_id = items.remove(0);
                 let test_result = items.join(" ");
-                data_arc.lock().unwrap().test_results.insert(test_id, TestResult::Pass(test_result));
+                data.test_results.insert(test_id, TestResult::Pass(test_result));
             },
             "fail" => {
                 let test_id = items.remove(0);
                 let test_result = items.join(" ");
-                data_arc.lock().unwrap().test_results.insert(test_id, TestResult::Fail(test_result));
+                data.test_results.insert(test_id, TestResult::Fail(test_result));
             },
             "skip" => {
                 let test_id = items.remove(0);
                 let test_result = items.join(" ");
-                data_arc.lock().unwrap().test_results.insert(test_id, TestResult::Skipped(test_result));
+                data.test_results.insert(test_id, TestResult::Skipped(test_result));
             },
             "log" => {
                 let message_class = items.remove(0);
@@ -493,7 +499,7 @@ fn stdin_monitor(data_arc: Arc<Mutex<InterfaceState>>, logs: Arc<Mutex<Vec<LogMe
                 logs.lock().unwrap().push(log_message.clone());
 
                 // Also add the new message to the list of current log messages.
-                data_arc.lock().unwrap().current_log.push(log_message);
+                data.current_log.push(log_message);
             },
             "exit" => std::process::exit(0),
             other => eprintln!("Unrecognized command: {}", other),
@@ -525,6 +531,11 @@ fn main() {
                                 .default_value("3000")
                                 .required(true)
                         )
+                        .arg(Arg::with_name("LOG_STDIN")
+                                .short("l")
+                                .long("log-stdin")
+                                .help("Enable logging stdin to /stdin.txt")
+                        )
                         .get_matches();
 
     let interface = matches.value_of("ADDRESS").unwrap();
@@ -545,6 +556,7 @@ fn main() {
         test_descriptions: HashMap::new(),
         test_results: HashMap::new(),
         stdin_log: vec![],
+        log_stdin: matches.is_present("LOG_STDIN"),
         current_log: vec![],
         previous_log: vec![],
     }));
@@ -562,10 +574,10 @@ fn main() {
     mnt.mount("/stdin.txt", move |request: &mut Request| show_stdin(request, &tmp_state));
 
     let tmp_state = state.clone();
-    mnt.mount("/current.json", move |request: &mut Request| show_current_logs_json(request, &tmp_state));
+    mnt.mount("/log/current.json", move |request: &mut Request| show_current_logs_json(request, &tmp_state));
 
     let tmp_state = state.clone();
-    mnt.mount("/previous.json", move |request: &mut Request| show_previous_logs_json(request, &tmp_state));
+    mnt.mount("/log/previous.json", move |request: &mut Request| show_previous_logs_json(request, &tmp_state));
 
     let tmp_state = state.clone();
     mnt.mount("/start", move |request: &mut Request| start_tests(request, &tmp_state));
